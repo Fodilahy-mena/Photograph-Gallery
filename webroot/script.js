d0d64e1de6c175329a9618a87815a8e40f258689
JEROME Emilien <jerome.emi@onja.org>
2021-03-20 18:27:18 +0300

    updated styling to be accessible to any devices, and added some vanilla js

--- webroot/script.js
+++ webroot/script.js
@@ -1,25 +1,47 @@
-console.log('Slide images');
 
 function Slide(slide) {
   // throw an error ('No Slide is found)
-  if(!slide) {
-    throw Error('No Slde is sound');
-  }
+  // if(!slide) {
+  //   throw Error('No Slde is sound');
+  // }
 
   // select all the images once by class
 
-  const images = Array.from(slide.querySelectorAll('.img'));
-  console.log(images);
+  // const images = Array.from(slide.querySelectorAll('.img'));
+  // console.log(images);
 
   const activeImg = document.querySelector('.ative--img');
   const prevButton = activeImg.querySelector('.prev');
   const nextButton = activeImg.querySelector('.next');
-  let currentImage;
 
   // event listeners to be bound
-  window.addEventListener('keyup', handleKeyUp);
-  nextButton.addEventListener('click', showNextImage);
-  prevButton.addEventListener('click', showPrevImage);
+window.addEventListener('keyup', handleKeyUp);
+nextButton.addEventListener("click", showNextImage = () => {
+  nextImages(+1);
+  let container = document.querySelector('.image-container');
+    sideScroll(container,'right',25,100,10);
+});
+
+prevButton.addEventListener("click", showPrevImage = () => {
+  nextImages(-1);
+  let container = document.querySelector('.image-container');
+    sideScroll(container,'left',25,100,10);
+});
+
+function sideScroll(element,direction,speed,distance,step){
+  scrollAmount = 0;
+  let slideTimer = setInterval(function(){
+      if(direction == 'left'){
+          element.scrollLeft -= step;
+      } else {
+          element.scrollLeft += step;
+      }
+      scrollAmount += step;
+      if(scrollAmount >= distance){
+          window.clearInterval(slideTimer);
+      }
+  }, speed);
+}
 
   // Enable the use of Arrow keys left and right as the same use of previous button and next button
   function handleKeyUp(e) {
@@ -27,37 +49,42 @@ function Slide(slide) {
     if(e.key === "ArrowRight") return showPrevImage();
 }
 
-function showNextImage(e) {
-  showImage(currentImage.nextElementSibling || slide.firstElementChild);
-};
+const slideImages = document.querySelectorAll('.image_slide');
+const sliders = document.querySelectorAll('.image_slider');
 
-function showPrevImage(e) {
-  showImage(currentImage.previousElementSibling || slide.lastElementChild);
-};
+let slideIndex = 1;
 
-function showImage(el) {
+const showImages = (n) => {
+  let i;
   
-  // update the the source of image inside the figure with this information
-  console.log(el);
-  activeImg.querySelector('img').src = el.src;
-  // store the reference of the current image
-  currentImage = el;
+  if (n > slideImages.length) {
+     slideIndex = 1 
+  }  
+
+  if(n < 1 ) {
+    slideIndex = slideImages.length
+  };
+
+  for (i = 0; i < slideImages.length; i++) {
+    slideImages[i].style.display = "none";
+  }
+
+  for (i = 0; i < sliders.length; i++) {
+    sliders[i].className = sliders[i].className.replace(" active", "");
+  }
+
+  slideImages[slideIndex-1].style.display = "block";
+  sliders[slideIndex-1].className += " active"; 
 }
 
-// loop through each of the images
-images.forEach(image => {
-  image.addEventListener('click', e => showImage(e.currentTarget));
-});
+showImages(slideIndex);
 
-images.forEach(image => {
-  image.addEventListener('keyup', e => {
-      
-      if(e.key === 'Enter') {
-          showImage(e.currentTarget);
-      }
-  });
-  
-  
+const nextImages = (n) => {
+  showImages(slideIndex += n);
+}
+
+sliders.forEach((image, index) => {
+  image.addEventListener('click', e => showImages(slideIndex = index + 1));
 });
 
 }
